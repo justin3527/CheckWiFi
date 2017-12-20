41055d21f3fb0448e3daecfa396302d5c7d22bfb
naver <naver@naverui-macbook-air-171.local>
2017-12-20 13:20:53 +0900

    Initial Commit

---
+++ CheckWiFi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckWiFi
+//
+//  Created by naver on 2017. 12. 20..
+//  Copyright © 2017년 ansi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
